07876814c79fdac3416887ca45e333e7e278cd66
Michal Prihoda <michal@prihoda.net>
2022-06-20 22:07:27 +0200

    macbook fixes

--- machines/macbook/overlays/httpie_darwin_fix.nix
+++ machines/macbook/overlays/httpie_darwin_fix.nix
@@ -4,6 +4,7 @@ self: super: {
       ipython = pySuper.ipython.overridePythonAttrs (old: {
         disabledTests = super.lib.optionals super.stdenv.isDarwin [
           "test_clipboard_get" # uses pbpaste
+          "test_request_body_from_file_by_path_with_explicit_content_type"
         ];
       });
     };
