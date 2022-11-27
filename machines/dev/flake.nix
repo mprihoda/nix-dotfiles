1d2b3b1b68ddd6d086c02b7e7124b76a79c6b4b1
Michal Příhoda <michal@prihoda.net>
2022-11-27 22:06:58 +0100

    Update dev emacs setup & vs code integration.

--- machines/dev/flake.nix
+++ machines/dev/flake.nix
@@ -31,10 +31,7 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [
-          ./home.nix
-          (import ../../modules/doom-emacs {})
-        ];
+        modules = [ ./home.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
